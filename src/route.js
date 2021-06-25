a9a5f8fd7b6903b67f4f7c19f8284fe9e1a8b5a7
Oseas Andre <oseasx65@gmail.com>
2021-06-24 21:03:02 -0300

    iniciando persistência; cadastrando room

--- src/route.js
+++ src/route.js
@@ -1,12 +1,17 @@
 const express = require('express')
+const QuestionControler = require('./controllers/QuestionController')
+const RoomController = require('./controllers/RoomController')
 
 const route = express.Router();
 
-route.get('/', (req, res) => res.render('index') )
-route.get('/room', (req, res) => res.render('room'))
-route.get('/create-pass', (req, res) => res.render('create-pass'))
+route.get('/', (req, res) => res.render('index', {page: 'enter-room'}) )
+route.get('/create-pass', (req, res) => res.render('index', {page: 'create-pass'}))
+
+route.get('/room/:room', (req, res) => res.render('room'))
+
 
 //Formato que o formulario de dentro da modal tem que passar informação
-//route.post('/room/:room/:question/:action')
+route.post('/question/:room/:question/:action', QuestionControler.index)
+route.post('/create-room', RoomController.create)
 
 module.exports = route;
